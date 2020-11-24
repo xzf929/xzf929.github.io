034cd9eb7db153c4f1a3127b466d2eb819186a1e
xzf929 <xzfahc@gmail.com>
2020-11-24 12:00:46 +0800

    time

---
+++ time/time.js
@@ -0,0 +1,26 @@
+function startTime(){
+    var td = new Date();
+    var y = td.getFullYear();
+    var m = td.getMonth()+1;
+    var d = td.getDate();
+    var h =td.getHours();
+    var mm = td.getMinutes();
+    var s = td.getSeconds();
+    m = checkTime(m);
+    d = checkTime(d);
+    h = checkTime(h);
+    mm = checkTime(mm);
+    s = checkTime(s);
+    document.getElementById("demo").innerHTML = 
+    y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + s;
+    // setTimeout({startTime()}, 500);
+}
+
+function checkTime(i){
+    if(i<10){
+        i = "0" + i;
+    }
+    return i;
+}
+
+window.setInterval("startTime()",1000);
